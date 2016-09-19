9d2c0547d37ba566a0b86146f4b5704a81d9b9c4
arek <arek@articode.pl>
2016-09-19 15:20:40 +0200

    1.0

---
+++ helper.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @copyright	Copyright (c) 2016 HJ. All rights reserved.
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+/**
+ * HJ - HJ Archive Helper Class.
+ *
+ * @package		Joomla.Site
+ * @subpakage	HJ.HJArchive
+ */
+class modHJArchiveHelper {
+	
+}
